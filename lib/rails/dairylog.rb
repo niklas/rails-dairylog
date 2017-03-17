6c18d3dd50b6ea09e45a054bd1c48f56c23be26d
Niklas Hofer <niklas+dev@lanpartei.de>
2017-03-17 18:15:50 +0100

    A cow a day keeps the goat away.

--- lib/rails/dairylog.rb
+++ lib/rails/dairylog.rb
@@ -2,6 +2,27 @@ require "rails/dairylog/version"
 
 module Rails
   module Dairylog
-    # Your code goes here...
+    class CowLoggerFormatter < ActiveSupport::Logger::SimpleFormatter
+      include ActiveSupport::TaggedLogging::Formatter
+      def initialize
+        super
+        if system("which cowsay")
+          @sayer = `which cowsay`.chomp + ' -W 9001'
+        end
+      end
+      def call(severity, time, progname, msg)
+        if @sayer && msg.present?
+          emsg = escape_for_shell msg
+          cmsg = "\n" + `#{@sayer} "#{emsg}"`
+          super severity, time, progname, cmsg
+        else
+          super
+        end
+      end
+
+      def escape_for_shell(str)
+        str.gsub('"', '\\"').strip
+      end
+    end
   end
 end
