b572088ec3f0e1726e510b2e636bff39b5518b42
Niklas Hofer <niklas+dev@lanpartei.de>
2017-03-18 12:23:33 +0100

    cowception with max level of MOO_FACTOR env var

--- lib/rails/dairylog.rb
+++ lib/rails/dairylog.rb
@@ -15,13 +15,23 @@ module Rails
         super
         if system("which cowsay &>/dev/null")
           @sayer = `which cowsay`.chomp + ' -n'
+          @moo_factor = (ENV.fetch('MOO_FACTOR') { 1 }).to_i
+          raise("cannot MOO #{@moo_factor} times") unless @moo_factor > 0
         end
       end
       def call(severity, time, progname, msg)
         if @sayer && msg.present?
-          super severity, time, progname, cowsay(cowsay(msg))
+          super severity, time, progname, gossip(msg, 1 + rand(@moo_factor))
         else
           super
+        end
+      end
+
+      def gossip(str, moo)
+        if moo == 1
+          cowsay(str)
+        else
+          gossip(cowsay(str), moo-1)
         end
       end
 
